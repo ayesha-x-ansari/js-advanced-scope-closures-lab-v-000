896156dc6485bacc52164013c8748afab243c0a7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-21 17:40:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function produceDrivingRange() {
+
+}
+
+function produceTipCalculator() {
+
+}
+
+function createDriver() {
+  driverId = 0
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId;
+    }
+
+  }
+}
